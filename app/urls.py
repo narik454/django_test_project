3ee5d958bab260b99bb52bac9bc79c815c10a471
narik454 <narik454@yandex.ru>
2024-08-08 00:45:41 +0300

    fix images and sells, add templatetags

--- app/urls.py
+++ app/urls.py
@@ -18,6 +18,9 @@ Including another URLconf
 from django.contrib import admin
 from debug_toolbar.toolbar import debug_toolbar_urls
 from django.urls import include, path
+from django.conf.urls.static import static
+
+from app import settings
 
 
 
@@ -25,4 +28,9 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls', namespace='main')),
     path('catalog/', include('goods.urls', namespace='catalog'))
-] + debug_toolbar_urls()
+
+]
+
+if settings.DEBUG:
+    urlpatterns += debug_toolbar_urls()
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- main/views.py
+++ main/views.py
@@ -1,13 +1,11 @@
 from django.shortcuts import render
-from goods.models import Categories
 
 def index(request):
-    categories = Categories.objects.all()
+    
 
     context = {
         'title': 'Чёрное Белое - Главная',
         'content': 'Фотостудия Чёрное Белое',
-        'categories': categories
     }
     return render(request, 'main/index.html', context)
 
